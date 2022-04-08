fa1c6961df14bbb08546f9a84ee1fdfaa2046b2c
Xueliang Chen <51306962+cxlgo64@users.noreply.github.com>
2022-04-07 20:12:27 -0400

    setup1
    Setup with vue and backend Json and javascript handle

---
+++ cart-backend/server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const PRODUCT_DATA_FILE = path.join(__dirname, 'server-product-data.json');
+const CART_DATA_FILE = path.join(__dirname, 'server-cart-data.json');
+
+app.set('port', (process.env.PORT || 3000));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+    res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+    res.setHeader('Pragma', 'no-cache');
+    res.setHeader('Expires', '0');
+    next();
+});
+
+app.post('/cart', (req, res) => {
+    fs.readFile(CART_DATA_FILE, (err, data) => {
+        const cartProducts = JSON.parse(data);
+        const newCartProduct = {
+            id: req.body.id,
+            title: req.body.title,
+            description: req.body.description,
+            price: req.body.price,
+            image_tag: req.body.image_tag,
+            quantity: 1
+        };
+        let cartProductExists = false;
+        cartProducts.map((cartProduct) => {
+            if (cartProduct.id === newCartProduct.id) {
+                cartProduct.quantity++;
+                cartProductExists = true;
+            }
+        });
+        if (!cartProductExists) cartProducts.push(newCartProduct);
+        fs.writeFile(CART_DATA_FILE, JSON.stringify(cartProducts, null, 4), () => {
+            res.setHeader('Cache-Control', 'no-cache');
+            res.json(cartProducts);
+        });
+    });
+});
+
+app.listen(app.get('port'), () => {
+    console.log(`Find the server at: http://localhost:${app.get('port')}/`);
+});
